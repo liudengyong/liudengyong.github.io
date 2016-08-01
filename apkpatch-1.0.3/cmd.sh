2146e00ec45ae7ffb6839127c18b1df6d1fe07da
刘登勇 <liudengyong@163.com>
2016-08-01 22:36:23 +0800

    制作补丁命令

--- apkpatch-1.0.3/cmd.sh
+++ apkpatch-1.0.3/cmd.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
-./apkpatch.sh -k ../../Doctorx/app/keystore/release.jks -p liudengyong -a doctorx -e liudengyong -f new.apk -t old.apk -o . -n doctorx.apatch
+#!/bin/sh
+
+code="10"
+name="app-JOJO-release_1.0.8_"
+name_code="$name""$code"
+old_apk="$name_code.apk"
+new_apk="$name_code""_fix.apk"
+./apkpatch.sh -k ../../Doctorx/app/keystore/release.jks -p liudengyong -a doctorx -e liudengyong -f $new_apk -t $old_apk -o ../ -n $code.apatch
 
