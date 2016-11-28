d55babe1114827ff6e3c8e30e75bd3dab7e8a10d
liudengyong <liudengyong@jd.com>
2016-11-28 15:06:10 +0800

    Revert "制作补丁命令"
    This reverts commit 2146e00ec45ae7ffb6839127c18b1df6d1fe07da.

--- apkpatch-1.0.3/cmd.sh
+++ apkpatch-1.0.3/cmd.sh
@@ -1,9 +1,3 @@
-#!/bin/sh
-
-code="10"
-name="app-JOJO-release_1.0.8_"
-name_code="$name""$code"
-old_apk="$name_code.apk"
-new_apk="$name_code""_fix.apk"
-./apkpatch.sh -k ../../Doctorx/app/keystore/release.jks -p liudengyong -a doctorx -e liudengyong -f $new_apk -t $old_apk -o ../ -n $code.apatch
+#!/bin/bash
+./apkpatch.sh -k ../../Doctorx/app/keystore/release.jks -p liudengyong -a doctorx -e liudengyong -f new.apk -t old.apk -o . -n doctorx.apatch
 
